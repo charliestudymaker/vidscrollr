4e58e2785d7ed61c3734d6d60bb2f434a2f06f99
Charles Johnson <cjohnson@Charless-Air.fios-router.home>
2021-05-04 00:57:01 -0400

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Background video - fullPage.js</title>
+	<title>VidScrollR</title>
 	<meta name="author" content="Alvaro Trigo Lopez" />
     <meta name="description" content="fullPage full-screen background videos." />
     <meta name="keywords"  content="fullpage,jquery,demo,screen,fullscreen,video,full-screen" />
@@ -97,51 +97,6 @@
 </head>
 <body>
 
-<select id="demosMenu">
-  <option selected>Choose Demo</option>
-  <option id="jquery-adapter">jQuery adapter</option>
-  <option id="active-slide">Active section and slide</option>
-  <option id="auto-height">Auto height</option>
-  <option id="autoplay-video-and-audio">Autoplay Video and Audio</option>
-  <option id="backgrounds">Background images</option>
-  <option id="backgrounds-fixed">Fixed fullscreen backgrounds</option>
-  <option id="background-video">Background video</option>
-  <option id="callbacks-v2-compatible">Callbacks version 2</option>
-  <option id="callbacks-v3">Callbacks version 3</option>
-  <option id="continuous-horizontal">Continuous horizontal</option>
-  <option id="continuous-vertical">Continuous vertical</option>
-  <option id="parallax">Parallax</option>
-  <option id="css3">CSS3</option>
-  <option id="drag-and-move">Drag And Move</option>
-  <option id="easing">Easing</option>
-  <option id="fading-effect">Fading Effect</option>
-  <option id="fixed-headers">Fixed headers</option>
-  <option id="gradient-backgrounds">Gradient backgrounds</option>
-  <option id="interlocked-slides">Interlocked Slides</option>
-  <option id="looping">Looping</option>
-  <option id="methods">Methods</option>
-  <option id="navigation-vertical">Vertical navigation dots</option>
-  <option id="navigation-horizontal">Horizontal navigation dots</option>
-  <option id="navigation-tooltips">Navigation tooltips</option>
-  <option id="no-anchor">No anchor links</option>
-  <option id="normal-scroll">Normal scrolling</option>
-  <option id="normalScrollElements">Normal scroll elements</option>
-  <option id="offset-sections">Offset sections</option>
-  <option id="one-section">One single section</option>
-  <option id="reset-sliders">Reset sliders</option>
-  <option id="responsive-auto-height">Responsive Auto Height</option>
-  <option id="responsive-height">Responsive Height</option>
-  <option id="responsive-width">Responsive Width</option>
-  <option id="responsive-slides">Responsive Slides</option>
-  <option id="scrollBar">Scroll bar enabled</option>
-  <option id="scroll-horizontally">Scroll horizontally</option>
-  <option id="scrollOverflow">Scroll inside sections and slides</option>
-  <option id="scrollOverflow-reset">ScrollOverflow Reset</option>
-  <option id="lazy-load">Lazy load</option>
-  <option id="scrolling-speed">Scrolling speed</option>
-  <option id="trigger-animations">Trigger animations</option>
-  <option id="vue-fullpage">Vue-fullpage component</option>
-</select>
 
 <div id="fullpage">
 	<div class="section " id="section0">
@@ -176,7 +131,7 @@
 </div>
 
 <script type="text/javascript" src="dist/fullpage.js"></script>
-<script type="text/javascript" src="examples.js"></script>
+
 
 <script type="text/javascript">
     var myFullpage = new fullpage('#fullpage', {
@@ -190,7 +145,10 @@
   function UnMuteVid(n)
   {
       document.getElementById('myVideo'+n).muted=!document.getElementById('myVideo'+n).muted;
-      document.getElementById('vctrl'+n).style.display='none';
+      if (document.getElementById('myVideo'+n).muted)
+	  document.getElementById('vctrl'+n).style.display='block';
+      else
+	  document.getElementById('vctrl'+n).style.display='none';
   }
 
   
